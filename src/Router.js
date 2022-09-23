3fcd711a809c30419664b4ffbd6d73d65c24d2dd
omarusmani <omarusmani3@hotmail.com>
2022-09-22 20:25:50 -0500

    cookies

--- src/Router.js
+++ src/Router.js
@@ -8,18 +8,27 @@ import Login from './components/Login'
 
 // Write checkAuth function here
 // Check the cookies for a cookie called "loggedIn"
-
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie);
+    return cookies["loggedIn"] ? true : false;
+  };
 
 // Write ProtectedRoute function here
+const ProtectedRoute = (props) => {
+    const { component: Component, ...rest } = props;
+    return (
+      checkAuth() === true ? ( <Component {...rest} /> ) : ( <Navigate to="/login" /> )
+    );
+  };
 
 
 const Router = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/car/:id" element={<Car/>} />
+            <Route path="/" element={<ProtectedRoute component={Home}/>} />
+            <Route path="/login" element= {<Login/>} />
+            <Route path="/about" element={<ProtectedRoute component={About}/>} />
+            <Route path="/car/:id" element={<ProtectedRoute component={Car}/>} />
         </Routes>
     );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { TextField, Button, Container } from "@mui/material";
-
+import cookie from 'cookie';
 const Login = () => {
   const navigate = useNavigate();
 
@@ -23,8 +23,8 @@ const Login = () => {
   const login = (e) => {
     e.preventDefault();
     // set cookie here
+    document.cookie = cookie.serialize("loggedIn", "true", { maxAge: 1000 * 60 });
     // set loggedIn = true and max-age = 60*1000 (one minute)
-
     navigate("/");
   };
 
